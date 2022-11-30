672e9fecc4311b2fd8bbb1d7c774cc85c0bf3a3a
S-D-ESPECHE <espeche.s.d@gmail.com>
2022-11-30 10:45:55 -0300

    Actualizacion3

--- src/main/java/com/portfolio/mgb/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/mgb/Security/Controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping({"/auth"})
-@CrossOrigin
+@CrossOrigin(origins = "https://frond-end-01.firebaseapp.com")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
