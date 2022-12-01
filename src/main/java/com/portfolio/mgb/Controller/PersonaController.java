51e4fb105d75465c261edbb6fd7ac275049823d1
S-D-ESPECHE <espeche.s.d@gmail.com>
2022-12-01 07:52:44 -0300

    Actualizacion13

--- src/main/java/com/portfolio/mgb/Controller/PersonaController.java
+++ src/main/java/com/portfolio/mgb/Controller/PersonaController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @CrossOrigin(origins = "https://frond-end-01.web.app")
 public class PersonaController {
